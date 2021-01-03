b3679b67f0d44a9d6484c292c57c83e27f82ac1a
bluepigwx <bluepigwx@hotmail.com>
2021-01-03 15:44:48 +0800

    simple server and cleint

--- simple_client_main.py
+++ simple_client_main.py
@@ -1,3 +1,24 @@
 # this is client
 
-print("hello")
+import socket
+import sys
+
+SERVER_PORT=9999
+
+client_socket = socket.socket()
+host = socket.gethostname()
+
+client_socket.connect((host, SERVER_PORT))
+
+client_run = True
+while client_run:
+    data = input(">>").strip()
+    if not data:
+        client_run = False
+        break
+    
+    client_socket.send(data.encode("utf-8"))
+
+client_socket.close()
+
+print("finish client")
--- simple_server_main.py
+++ simple_server_main.py
@@ -1,1 +1,31 @@
 # this is server
+
+import socket
+import sys
+
+
+MAX_LISTEN_QUEUE=5
+SERVER_PORT=9999
+
+host_name = socket.gethostname()
+print('server host name', host_name)
+
+server_socket = socket.socket()
+server_socket.bind((host_name, SERVER_PORT))
+
+server_socket.listen(MAX_LISTEN_QUEUE)
+
+print("server begin run")
+server_run = True
+while server_socket :
+    client_socket, addr = server_socket.accept()
+    print("new client addr", str(addr))
+
+    server_run = False
+
+    client_socket.close()
+
+server_socket.close()
+
+print("server finish")
+    
